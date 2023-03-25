e06fc27a914caed55dd023f2eb382118cbc94ae5
Mukesh Pandey <mukeshpandey9017@gmail.com>
2023-03-25 17:39:17 +0530

    adding Search bar utility on store page

--- zipkart/store/views.py
+++ zipkart/store/views.py
@@ -4,6 +4,8 @@ from category.models import category
 from cart.models import CartItem
 from cart.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from django.db.models import Q
+from django.http import HttpResponse
 
 # Create your views here.
 def store(request,category_slug=None):
@@ -11,7 +13,7 @@ def store(request,category_slug=None):
     products = None
     if category_slug != None:
         categories = get_object_or_404(category,slug=category_slug)
-        products = Product.objects.all().filter(category=categories,is_available=True)
+        products = Product.objects.all().filter(category=categories,is_available=True).order_by("id")
         paginator = Paginator(products,1)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -43,3 +45,15 @@ def product_detail(request,category_slug,product_slug):
         "in_cart":in_cart
     }
     return render(request,"store/product_detail.html",context=context)
+
+def search(request):
+    if "search" in request.path:
+        keyword = request.GET["keyword"]
+        if keyword:
+            products = Product.objects.all().order_by("modified_date").filter(Q(product_name__icontains=keyword)|Q(description__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products':products,
+        'product_count': product_count
+    }
+    return render(request,"store/store.html",context=context)
